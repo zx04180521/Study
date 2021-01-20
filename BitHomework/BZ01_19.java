16b0f4f8fc090167c04b43bc804b3eff6217966d
zx04180521 <1281354107@qq.com>
2021-01-20 10:44:24 +0800

    作业

---
+++ BitHomework/BZ01_19.java
@@ -0,0 +1,36 @@
+package BitHomework;
+
+public class BZ01_19 {
+    public static void main(String[] args) {
+//        Hello h=null;
+//        h.hello();
+
+//        String s;
+//        System.out.println("s=:"+s);
+
+//        Test test = new Test();
+//        test.aMethod();
+//        int j = test.aMethod();
+//        System.out.println(j);
+
+//        System.out.println(Paddy);
+
+
+
+    }
+
+    static boolean Paddy;
+}
+class Hello{
+    public static void hello(){
+        System.out.println("hello");
+    }
+}
+class Test{
+    public int aMethod(){
+         int i = 0;
+        i++;
+        return i;
+    }
+}
+
