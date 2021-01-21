8086f79f00599e1e7fdda4736b5201284b8c15c3
zx04180521 <1281354107@qq.com>
2021-01-21 12:02:15 +0800

    类多态的学习

---
+++ Object/Dog.java
@@ -0,0 +1,7 @@
+package Object;
+
+public class Dog extends Animal{
+    public Dog(String name){
+        super(name);
+    }
+}
---
+++ Object/Test.java
@@ -0,0 +1,26 @@
+package Object;
+/*
+*动态绑定
+**/
+
+class A {
+    public A() {
+        fun();//触发动态绑定，调用A里面的fun
+    }
+
+    public void fun() {
+        System.out.println("A.fun");
+    }
+}
+
+class B extends A {
+    int num = 1;
+    public void fun(){
+        System.out.println("B.fun "+num);
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        B t = new B();
+    }
+}
