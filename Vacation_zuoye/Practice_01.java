b4e0fa798160fcc84065838fbbae7ec458a33221
zx04180521 <1281354107@qq.com>
2021-01-30 23:13:57 +0800

    刷题

--- Vacation_zuoye/Practice_01.java
+++ Vacation_zuoye/Practice_01.java
@@ -4,27 +4,34 @@ import java.util.Arrays;
 
 public class Practice_01 {
     public static void main(String[] args) {
-        String s = "A man, a plan, a canal: Panama";
-        System.out.println(isPalindrome(s));
+        int[] nums={1,2,3,5,6,4,7};
+        System.out.println(findUnsortedSubarray(nums));
+    }
+    public static int  findUnsortedSubarray(int[] nums){
+        int left=0;
+        int right=0;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+            else if(nums[i]<max) {
+                right=i;
+            }
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<min){
+                min=nums[i];
+            }else if(nums[i]>min){
+                left=i;
+            }
+        }
+        if(left==right){
+            return 0;
+        }
+        return right-left+1;
     }
 
-    public static boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-            String str1=String.valueOf(s.charAt(left));
-            String str2=String.valueOf(s.charAt(right));
-            if (!str1.equalsIgnoreCase(str2))
-                return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
+
 }
