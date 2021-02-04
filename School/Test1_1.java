ec9ea58c820f23ccb6b217b7e6e19a35b1b834d9
zx04180521 <1281354107@qq.com>
2021-02-04 22:12:15 +0800

    作业

---
+++ School/Test1_1.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Test1_1 {
+	/**
+	 * 软件1801 张鑫     
+	 * ID:1806050126
+	 */
+	public static void main(String[] args) {
+	
+		
+
+		System.out.println("撸起袖子加油干，The nation remains mobilized  for brand new endeavors");
+
+		
+	}
+}
