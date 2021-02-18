134608ccb6405e35ea09273f06befe6f35adac26
zx04180521 <1281354107@qq.com>
2021-02-18 22:41:55 +0800

    作业

---
+++ School/Example4_11.java
@@ -0,0 +1,21 @@
+package four;
+
+public class Example4_11 {
+
+	public static void main(String[] args) {
+		new Tomm(4);
+	}
+
+}
+ class Tomm{
+	int leg;
+	Tomm(int n){
+		this.cry();
+		leg=n;
+		this.cry();
+	}
+void cry() {
+	System.out.println("我是Tom，我现在有"+leg+"条腿");
+	
+}	
+}
