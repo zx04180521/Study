e09fe2251cb9ad9f6a0107d4236fe2d9ea1cfe03
zx04180521 <1281354107@qq.com>
2021-03-13 15:41:19 +0800

    作业

---
+++ HomeworkObject/Test.java
@@ -0,0 +1,14 @@
+package HomeworkObject;
+
+public class Test{
+    static int cnt = 6;
+    static{
+        cnt += 9;
+    }
+    public static void main(String[] args){
+        System.out.println("cnt =" + cnt);
+    }
+    static{
+        cnt /=3;
+    };
+}
