4d2bfa81616b719f458217605635f55ff1f39a9a
zx04180521 <1281354107@qq.com>
2021-04-02 14:03:04 +0800

    力扣

---
+++ LeetCode/Offer_10_1.java
@@ -0,0 +1,33 @@
+package LeetCode;
+/*斐波那契数列*/
+/*写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1
+。*/
+public class Offer_10_1 {
+    public static void main(String[] args) {
+
+    }
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        int c=0;
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else{
+            for(int i=0;i<n-1;i++){
+                c=(a+b)%1000000007;
+                a=b;
+                b=c;
+            }
+        }
+        return c;
+
+    }
+}
