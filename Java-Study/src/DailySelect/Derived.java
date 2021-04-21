423792993e4c8a81e778f548fda4d55ee8f5a74e
zx04180521 <1281354107@qq.com>
2021-04-21 20:51:12 +0800

    代码

---
+++ Java-Study/src/DailySelect/Derived.java
@@ -0,0 +1,18 @@
+package DailySelect;
+
+class Base{
+    public Base(String s){
+        System.out.print("B");
+    }
+
+}
+public class Derived extends Base {
+    public Derived(String s) {
+        super(s);
+        System.out.print("D");
+    }
+
+    public static void main(String[] args) {
+        new Derived("C");
+    }
+}
