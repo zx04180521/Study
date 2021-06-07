57a64be54e529e8a182aad2f467d134ad302b53d
zx04180521 <1281354107@qq.com>
2021-06-08 00:00:19 +0800

    1

---
+++ Java-Study/src/DailySelect/Print.java
@@ -0,0 +1,16 @@
+package DailySelect;
+
+public class Print {
+    static boolean out(char c) {
+        System.out.print(c);
+        return true;
+    }
+
+    public static void main(String[] argv) {
+        int i = 0;
+        for (out('A'); out('B') && (i < 2); out('C')) {
+            i++;
+            out('D');
+        }
+    }
+}
