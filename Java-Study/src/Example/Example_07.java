92e3bdcdfea78aa537af98796e5dbad40b51a06f
zx04180521 <1281354107@qq.com>
2021-06-10 22:17:52 +0800

    1

---
+++ Java-Study/src/Example/Example_07.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class Example_07 {
+    public static void main(String[] args) {
+        int a=123;
+        System.out.printf("%d\n",a);
+        System.out.printf("%4d\n",a);
+        System.out.printf("%04d\n",a);
+    }
+}
