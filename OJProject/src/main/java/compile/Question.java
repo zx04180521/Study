52544c8318ca72459648f53faed614b4c87fefae
zx04180521 <1281354107@qq.com>
2021-06-25 00:33:45 +0800

    1

---
+++ OJProject/src/main/java/compile/Question.java
@@ -0,0 +1,16 @@
+package compile;
+
+//表示编译运行的输入
+public class Question {
+    //编译运行的代码
+    //用户在网页上编辑的代码
+    private String code;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
