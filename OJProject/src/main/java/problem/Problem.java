9cfbc960df63b3dd1e5f0e45c61a397c910e980c
zx04180521 <1281354107@qq.com>
2021-06-25 00:34:02 +0800

    1

---
+++ OJProject/src/main/java/problem/Problem.java
@@ -0,0 +1,77 @@
+package problem;
+
+public class Problem {
+    //题目的编号
+    private int id;
+    //题目的标题
+    private  String title;
+    //题目的难度
+    private String level;
+    //题目的详细信息
+    private String description;
+    //题目的代码模板
+    private String templateCode;
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", level='" + level + '\'' +
+                ", description='" + description + '\'' +
+                ", templateCode='" + templateCode + '\'' +
+                ", testCode='" + testCode + '\'' +
+                '}';
+    }
+
+    //题目的测试用例代码
+    private String testCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTemplateCode() {
+        return templateCode;
+    }
+
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode;
+    }
+
+    public String getTestCode() {
+        return testCode;
+    }
+
+    public void setTestCode(String testCode) {
+        this.testCode = testCode;
+    }
+}
