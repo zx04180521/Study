24afe9d9318c63bc61856b056b9726f0b69e376c
zx04180521 <1281354107@qq.com>
2021-08-10 01:25:52 +0800

    1

---
+++ Servlet-Study/src/main/java/org/Demo_TZ/ServletDemo8.java
@@ -0,0 +1,27 @@
+package org.Demo_TZ;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+//通过读取请求，获取到浏览器发来的Cookie内容
+@WebServlet("/ServletDemo8")
+public class ServletDemo8 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies=req.getCookies();
+        resp.setContentType("text/html;charset=utf-8");
+        Writer writer=resp.getWriter();
+        writer.write("<html>");
+        for(Cookie cookie:cookies){
+            writer.write(cookie.getName()+":"+cookie.getValue());
+            writer.write("<br>");
+        }
+        writer.write("<html>");
+    }
+}
